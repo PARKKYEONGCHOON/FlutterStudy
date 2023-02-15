fc71a05c4c2257ee7ab5be89cddd6d060aaf68a1
PARKKYEONGCHOON <98440576+PARKKYEONGCHOON@users.noreply.github.com>
2023-02-15 18:47:00 +0900

    Add files via upload

---
+++ listview_lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'sub/firstPage.dart';
+import 'sub/secondPage.dart';
+import './animalItem.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  TabController? controller;
+  List<Animal> animalList = new List.empty(growable: true);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('ListView Exam'),
+        ),
+        body: TabBarView(
+          children: <Widget>[FirstApp(list: animalList), SecondApp()],
+          controller: controller,
+        ),
+        bottomNavigationBar: TabBar(
+          tabs: [
+            Tab(
+              icon: Icon(Icons.looks_one, color: Colors.blue),
+            ),
+            Tab(
+              icon: Icon(Icons.looks_two, color: Colors.blue),
+            )
+          ],
+          controller: controller,
+        ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+
+    animalList.add(
+        Animal(animalName: "벌", kind: "곤충", imagePath: "repo/images/bee.png"));
+    animalList.add(Animal(
+        animalName: "고양이", kind: "포유류", imagePath: "repo/images/cat.png"));
+    animalList.add(
+        Animal(animalName: "소", kind: "포유류", imagePath: "repo/images/cow.png"));
+    animalList.add(Animal(
+        animalName: "강아지", kind: "포유류", imagePath: "repo/images/dog.png"));
+    animalList.add(Animal(
+        animalName: "여우", kind: "포유류", imagePath: "repo/images/fox.png"));
+    animalList.add(Animal(
+        animalName: "원숭이", kind: "포유류", imagePath: "repo/images/monkey.png"));
+    animalList.add(Animal(
+        animalName: "돼지", kind: "포유류", imagePath: "repo/images/pig.png"));
+    animalList.add(Animal(
+        animalName: "늑대", kind: "포유류", imagePath: "repo/images/wolf.png"));
+  }
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+}
